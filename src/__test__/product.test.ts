695d7bc7075ba6922c9e3f06c4c93757f9b6b398
adeelkbw <adeelnasirkbw@gmail.com>
2022-08-14 00:01:15 +0500

    dummy product test

---
+++ src/__test__/product.test.ts
@@ -0,0 +1,9 @@
+describe('product', () => {
+  describe('get product route', () => {
+    describe('given product does not exits', () => {
+      it('should return 404', () => {
+        expect(true).toBe(true)
+      })
+    })
+  })
+})
